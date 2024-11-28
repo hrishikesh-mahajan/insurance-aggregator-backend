00419e4082264fa0c99575228f47e80da965d178
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2024-11-29 02:21:50 +0530

    feat: implement form submission with file handling and MongoDB integration, add HTML form for user input

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, make_response, request
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -5,32 +7,30 @@ from pymongo import MongoClient
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
+
 
 @app.route("/submit-form", methods=["POST"])
 def submit_form():
-    form_data = request.form
+    form_data = request.form.to_dict()
+    files = request.files
 
-    images = request.files.getlist("images")
-    pdfs = request.files.getlist("pdfs")
+    # Combine form data and files into a single dictionary
+    data_to_insert = form_data.copy()
 
-    # Convert files to binary data
-    image_blobs = [image.read() for image in images]
-    pdf_blobs = [pdf.read() for pdf in pdfs]
+    for key in files:
+        uploaded_files = request.files.getlist(key)
+        # Store files as binary data (blobs)
+        data_to_insert[key] = [file.read() for file in uploaded_files]  # type: ignore
 
-    # Assuming you have a MongoDB collection called 'form_data'
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["insurance_aggregator"]
-    collection = db["form_data"]
-
-    # Insert form data and files into MongoDB
-    form_data_dict = form_data.to_dict()
-    form_data_dict["images"] = image_blobs  # type: ignore
-    form_data_dict["pdfs"] = pdf_blobs  # type: ignore
-
-    collection.insert_one(form_data_dict)
+    # Insert into MongoDB
+    client = MongoClient(os.getenv("MONGO_URI"))
+    db = client[os.getenv("MONGO_DB_NAME")]
+    collection = db[os.getenv("MONGO_COLLECTION_NAME")]
+    collection.insert_one(data_to_insert)
 
     return make_response("Form submitted successfully", 200)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, debug=True)
